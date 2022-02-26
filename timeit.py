1516eb5370f196793158ba8ddc5480afe9294e32
Evan Astle <starphys@gmail.com>
2022-02-26 15:30:39 -0800

    calculate_time decorator working and tested locally

---
+++ timeit.py
@@ -0,0 +1,10 @@
+import time
+
+def calculate_time(func) :
+    def wrapper(*args, **kwargs) :
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        total = end-start
+        print(f"Total time {total}")
+    return wrapper
