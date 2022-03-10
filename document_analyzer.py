0494e42d41529b4c56ce29bda1598c9a5d39edb5
Evan Astle <starphys@gmail.com>
2022-03-09 18:27:20 -0800

    document_analyzer improved sorting

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,15 +1,20 @@
-import re
-
-
 def list_top_values(dictionary, count):
     list_to_return = []
 
     for i in range(count):
         highest = max(dictionary, key=dictionary.get)
-        list_to_return.append(highest)
-        list_to_return.append(dictionary[highest])
+        list_to_return.append(f"{highest}: {dictionary[highest]}")
         del dictionary[highest]
+
     return list_to_return
+
+
+def lexographical_sort_dictionary(dictionary):
+    sorted_keys = sorted(dictionary, key=str.lower)
+    sorted_dict = {}
+    for key in sorted_keys:
+        sorted_dict[key] = dictionary.get(key)
+    return sorted_dict
 
 
 # Get all the text from a file into one string
@@ -29,8 +34,9 @@ word_dict = {}
 for word in word_list:
     word_dict[word] = word_dict.get(word, 0) + 1
 
+word_dict = lexographical_sort_dictionary(word_dict)
+
 top_words = list_top_values(word_dict, 5)
 
-print()
-for i in range(0, len(top_words), 2):
-    print(f"{top_words[i]}: {top_words[i+1]}")
+for i in range(0, len(top_words)):
+    print(top_words[i])
